4e544b9e935125a1f718d393b364cbe2e6fbac51
zhangyida <18859669020@163.com>
2018-11-28 22:02:37 +0800

    driver for 66ip

---
+++ app/Spiders/Drivers/Sixsixip.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Spiders\Drivers;
+
+use App\Spiders\Spider;
+
+class Sixsixip extends Spider
+{
+    public function handle()
+    {
+        $urls = [
+            "http://www.66ip.cn/1.html",
+            "http://www.66ip.cn/2.html",
+            "http://www.66ip.cn/3.html",
+            "http://www.66ip.cn/4.html",
+            "http://www.66ip.cn/5.html",
+            "http://www.66ip.cn/6.html",
+        ];
+
+        $this->process($urls, "#main table tr", function ($tr) {
+            $ip = $tr->find('td:eq(0)')->text();
+            $port = $tr->find('td:eq(1)')->text();
+            return $ip . ':' . $port;
+        });
+    }
+}
