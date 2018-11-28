06e877f9f5300f2aa5b1ce26a05b81d05dd0d2ac
zhangyd <18859669020@163.com>
2018-11-28 17:59:20 +0800

    driver for kuaidaili

---
+++ app/Spiders/Drivers/Kuaidaili.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Spiders\Drivers;
+
+use App\Spiders\Spider;
+
+class Kuaidaili extends Spider
+{
+    public function handle()
+    {
+        $this->sleep = 3;
+        $urls = [
+            "http://www.kuaidaili.com/free/inha/",
+            "http://www.kuaidaili.com/free/inha/2/",
+            "http://www.kuaidaili.com/free/inha/3/",
+            "http://www.kuaidaili.com/free/inha/4/",
+            "http://www.kuaidaili.com/free/inha/5/",
+            "http://www.kuaidaili.com/free/intr/",
+            "http://www.kuaidaili.com/free/intr/2/",
+            "http://www.kuaidaili.com/free/intr/3/",
+            "http://www.kuaidaili.com/free/intr/4/",
+            "http://www.kuaidaili.com/free/intr/5/",
+        ];
+        $this->process($urls, "#list table tr", function ($tr) {
+            $ip = $tr->find('td:eq(0)')->text();
+            $port = $tr->find('td:eq(1)')->text();
+            return $ip . ':' . $port;
+        });
+    }
+}
