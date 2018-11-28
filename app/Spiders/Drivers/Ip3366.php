7b43c79b83a2b94702dee654ab7dffdd324d4d5b
zhangyida <18859669020@163.com>
2018-11-28 22:02:23 +0800

    driver for ip3366

---
+++ app/Spiders/Drivers/Ip3366.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Spiders\Drivers;
+
+use App\Spiders\Spider;
+
+class Ip3366 extends Spider
+{
+    public function handle()
+    {
+        $urls = [
+            "http://www.ip3366.net/free/?stype=1&page=1",
+            "http://www.ip3366.net/free/?stype=1&page=2",
+            "http://www.ip3366.net/free/?stype=1&page=3",
+            "http://www.ip3366.net/free/?stype=1&page=4",
+            "http://www.ip3366.net/free/?stype=2&page=1",
+            "http://www.ip3366.net/free/?stype=2&page=2",
+            "http://www.ip3366.net/free/?stype=2&page=3",
+            "http://www.ip3366.net/free/?stype=2&page=4",
+            "http://www.ip3366.net/free/?stype=3&page=1",
+            "http://www.ip3366.net/free/?stype=3&page=2",
+            "http://www.ip3366.net/free/?stype=3&page=3",
+            "http://www.ip3366.net/free/?stype=3&page=4",
+            "http://www.ip3366.net/free/?stype=4&page=1",
+            "http://www.ip3366.net/free/?stype=4&page=2",
+            "http://www.ip3366.net/free/?stype=4&page=3",
+            "http://www.ip3366.net/free/?stype=4&page=4",
+        ];
+
+        $this->process($urls, "#list table tr", function ($tr) {
+            $ip = $tr->find('td:eq(0)')->text();
+            $port = $tr->find('td:eq(1)')->text();
+            return $ip . ':' . $port;
+        });
+    }
+}
