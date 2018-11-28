1c28b3f01aa8f91bb727910700d5741ddb6d6590
zhangyida <18859669020@163.com>
2018-11-28 22:18:41 +0800

    driver for data5u

---
+++ app/Spiders/Drivers/Data5u.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Spiders\Drivers;
+
+use App\Spiders\Spider;
+
+class Data5u extends Spider
+{
+    public function handle()
+    {
+        $urls = [
+            "http://www.data5u.com/free/index.shtml",
+            "http://www.data5u.com/free/gngn/index.shtml",
+            "http://www.data5u.com/free/gnpt/index.shtml",
+            "http://www.data5u.com/free/gwgn/index.shtml",
+            "http://www.data5u.com/free/gwpt/index.shtml"
+        ];
+
+        $this->process($urls, "ul.l2", function ($tr) {
+            $ip = $tr->find('li:eq(0)')->text();
+            $port = $tr->find('li:eq(1)')->text();
+            return $ip . ':' . $port;
+        });
+    }
+}
