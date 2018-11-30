e84cfb5d885e29798c68754a3c5edb29329b4de5
zhangyd <18859669020@163.com>
2018-11-30 16:12:47 +0800

    资源本地化

--- resources/views/layout/html.blade.php
+++ resources/views/layout/html.blade.php
@@ -5,16 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>代理池</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link href="https://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://adminlte.io/themes/AdminLTE/dist/css/AdminLTE.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
 <body >
 
 @yield('body')
 
-<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="https://cdn.bootcss.com/clipboard.js/1.5.16/clipboard.min.js"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/clipboard.min.js')}}"></script>
 
 @yield('js')
 </body>
