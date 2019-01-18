95151773e503bd69e673514717694161a88f1bb0
zhangyd <18859669020@163.com>
2019-01-18 11:34:37 +0800

    代理池上限调整

--- config/proxy.php
+++ config/proxy.php
@@ -5,7 +5,7 @@ return [
     'check_url' => env('PROXY_CHECK_URL', 'http://www.baidu.com/'),
     'check_keyword' => env('PROXY_CHECK_KEYWORD', '百度一下'),
 
-    'limit_count' => env('PROXY_LIMIT_COUNT', 5000),
+    'limit_count' => env('PROXY_LIMIT_COUNT', 2000),
 
     'timeout' => env('PROXY_TIME_OUT', 2),
     'connect_timeout' => env('PROXY_CONNECT_TIMEOUT', 2),
